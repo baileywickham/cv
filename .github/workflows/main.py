34d7adf9df0d67a6724e6baee9a090fa2efb03df
baileywickham <baileywickham@gmail.com>
2022-10-13 09:54:11 +0800

    Repush cv

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -2,7 +2,7 @@ from workflow import *
 
 on = On({'push': {'branches': ['main']}})
 steps = [Step(Uses('actions/checkout@v3')),
-         Step(Run('apt-get update && apt-get install -y curl gnupg wget libfontconfig python3')),
+         Step(Run('sudo apt-get update && sudo apt-get install -y curl gnupg wget libfontconfig python3')),
          Step(Run('wget https://raw.githubusercontent.com/baileywickham/files/master/phantomjs-2.1.1-linux-x86_64.tar.bz2')),
          Step(Run('tar -jxvf phantomjs-2.1.1-linux-x86_64.tar.bz2')),
          Step(Run('./toPDF.sh'))]
